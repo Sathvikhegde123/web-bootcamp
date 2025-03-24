9a78ed30cfd3f64ba129453ba6f228e5b24abdf3
Sathvikhegde123 <sathvik.hegde17@gmail.com>
2025-03-24 18:11:54 +0530

    test change

---
+++ one_more_new.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve(){
+   cout<<"github bartaide bro\n";
+
+}
+
+ int main(){
+    int t;
+    cin>>t;
+    while(t--){
+         solve();
+    }
+ }
